f6f7e24f9b87db10be3a72036bfb8e127c897f9e
jahidhasanm1 <jahidnsu142@gmail.com>
2024-11-18 09:35:15 +0300

    used lombok instead of traditional model

--- src/main/java/com/example/pinware/model/PinwareUser.java
+++ src/main/java/com/example/pinware/model/PinwareUser.java
@@ -1,12 +1,18 @@
 package com.example.pinware.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 import java.time.Period;
 
 @Entity
 @Table
+@Setter
+@Getter
+@NoArgsConstructor
 public class PinwareUser {
     @Id
     @SequenceGenerator(
@@ -23,68 +29,22 @@ public class PinwareUser {
     private  String userName;
     private  String gender;
     private  String email;
+    private  String country;
     private LocalDate dob;
 
     @Transient
     private Integer age;
 
-    public PinwareUser(String userName, String gender, String email, LocalDate dob) {
+    public PinwareUser(String userName, String gender, String email, String country, LocalDate dob) {
         this.userName = userName;
         this.gender = gender;
         this.email = email;
         this.dob = dob;
-    }
-
-    public PinwareUser() {
-
+        this.country = country;
     }
 
     public Integer getAge() {
         return Period.between(this.dob, LocalDate.now()).getYears();
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public LocalDate getDob() {
-        return dob;
-    }
-
-    public void setDob(LocalDate dob) {
-        this.dob = dob;
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     @Override
@@ -94,6 +54,7 @@ public class PinwareUser {
                 ", userName='" + userName + '\'' +
                 ", gender='" + gender + '\'' +
                 ", email='" + email + '\'' +
+                ", country='" + country + '\'' +
                 ", dob=" + dob +
                 '}';
     }
