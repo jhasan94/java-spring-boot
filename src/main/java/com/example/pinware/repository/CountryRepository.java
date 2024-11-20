911b68b28655dc24aca79ec22df634ace6698e4d
jahidhasanm1 <jahidnsu142@gmail.com>
2024-11-20 16:03:02 +0300

    added country repository

---
+++ src/main/java/com/example/pinware/repository/CountryRepository.java
@@ -0,0 +1,10 @@
+package com.example.pinware.repository;
+
+import com.example.pinware.model.Country;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CountryRepository extends  JpaRepository<Country,Long> {
+    
+}
